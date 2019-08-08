431c5be05f23b088664228a58b104e17991071fb
dxviidmg <dmg_92@hotmail.com>
2019-08-08 13:10:12 -0500

    optimizacion sms lista

--- ventas/views.py
+++ ventas/views.py
@@ -107,6 +107,7 @@ class CreateViewVenta(SuccessMessageMixin, CreateView):
 
 	def form_valid(self, form):
 		plan = Plan.objects.get(slug=self.kwargs['slug'])
+		tecnologia_wifi = plan.punto_venta.tecnologia_wifi
 		try:
 			codigo = Codigo.objects.filter(plan=plan, status="Disponible")[:1].get()
 			codigo.status = "Vendido"
@@ -114,16 +115,26 @@ class CreateViewVenta(SuccessMessageMixin, CreateView):
 	
 			form.instance.codigo = codigo
 
-			r1 = "Voucher: " + codigo.codigo
-			r2 = "\nTiempo: " + str(codigo.plan.duracion) + " " + codigo.plan.unidad_duracion
+			unidad_duracion = "hora"
+			if plan.unidad_duracion == "d":
+				unidad_duracion = "dia"
+
+			if plan.duracion > 1:
+				unidad_duracion = unidad_duracion + "s"
+
+			r1 = "Voucher: "
+			if tecnologia_wifi == "Mikrotik":
+				r1 = "User and password: "
+
+			r1 = r1 + codigo.codigo
+			r2 = "\nTiempo: " + str(codigo.plan.duracion) + " " + unidad_duracion
 			r3 = "\nRed: " + codigo.plan.punto_venta.nombre_red
 			r4 = "\n" + codigo.plan.punto_venta.nombre + " agradece su preferencia."
 			renglones = [r1, r2, r3, r4]
-#		renglones = [r1]
 
 			mensaje = " ".join(renglones)
 			print(mensaje)
-
+			
 			form.instance.status_sms = altiriaSms("52" + form.instance.telefono, mensaje, True)
 			plan.contar_codigos_disponibles()
 
@@ -144,5 +155,9 @@ class CreateViewVenta(SuccessMessageMixin, CreateView):
 		if plan.duracion > 1:
 			unidad_duracion = unidad_duracion + "s"
 		context['unidad_duracion'] = unidad_duracion
-		context['ultima_venta'] = Venta.objects.latest('pk')
+		try:
+			ultima_venta = Venta.objects.latest('pk')
+		except:
+			ultima_venta = None
+		context['ultima_venta'] = ultima_venta
 		return context
